7fc809e56c0868c17dc7c2afdc22f4ca7a8145f8
Eduardo <eduardo.rondon@apstem.com>
2020-10-07 12:23:25 +0200

    feat: model finished

--- backend/models.py
+++ backend/models.py
@@ -2,14 +2,14 @@ import os
 import json
 from datetime import datetime
 from sqlalchemy import Column, String, Integer, create_engine
-from flask_sqlalchemy import SQLAlchemy
+from  import SQLAlchemy
 
 
 database_path = os.environ.get('DATABASE_PATH') 
 
 db = SQLAlchemy()
 
-'''
+'''flask_sqlalchemy
 setup_db(app)
     binds a flask application and a SQLAlchemy service
 '''
@@ -19,7 +19,14 @@ def setup_db(app, database_path=database_path):
     db.app = app
     db.init_app(app)
     db.create_all()
-    return db
+    
+
+'''
+Configuration
+
+'''
+MAX_MOVIES_PER_PAGE = 10
+MAX_ACTORS_PER_PAGE = 10
 
 '''
 Models
@@ -97,7 +104,44 @@ class Movies(db.Model):
             }
     def __repr__(self):
         return f'{self.format()}'
+    @staticmethod
+    def search(searchTerm):
+        """Get the entire movies list for a certain search word
+        Keyword arguments:
+            searchTerm -- the String term to search in the name of the movies
+        """
+        search = f'%{searchTerm}%'
+        #query the movie with WHERE LIKE case insentive
+        movies = Movies.query.filter(Movies.name.ilike(search)).all()
+        if len(movies) == 0:
+            return {'movies':[]}
+        return { 'movies': [movie.format() for movie in movies] }
+    @staticmethod
+    def read(id):
+        """Get the movie for an id
+        Keyword arguments:
+            id -- the integer id of the movie
+        """
+        movie = Movies.query.get(id)
+        if movie is None:
+            return None
+        return movie.response()
+    @staticmethod
+    def read_all(page=0):
+        """Get all movies per page
+        Keyword arguments:
+            page -- the integer page number
+        """
+        movies = Movies.query.all()
+        total = len(movies)
 
+        if page == -1:
+            return { 'movies': [movie.format() for movie in movies], 'total': total, 'count': total}
+        offset = (page-1)*MAX_MOVIES_PER_PAGE
+        if offset > total:
+            return None
+        limit = min(offset + MAX_MOVIES_PER_PAGE,total-1)
+        return { 'movies': [movie.format() for movie in movies][offset:limit], 'total': total, 'count':limit-offset}
 
 class Actors(db.Model):
     __tablename__ = 'actors'
@@ -158,8 +202,54 @@ class Actors(db.Model):
             }
     def __repr__(self):
         return f'{self.format()}'
+    @staticmethod
+    def search(searchName=None,searchGender=None,searchAge=None):
+        """Get the entire actor list for a given combination of search terms
+        Keyword arguments:
+            searchName -- the String term to search in the name of the actors
+            searchGender -- the String term to search in the gender of the actors
+            searchAge -- the Integer value to search in the age of the actors
+        """
+        if searchName == None and searchGender == None and searchAge == None:
+            return {'actors':[]}
+        query_search = Actors.query
+        if searchName != None:
+            search = f'%{searchName}%'
+            query_search = query_search.filter(Actors.name.ilike(search))
+        if searchGender != None:
+            query_search = query_search.filter(Actors.gender == searchGender)
+        if searchAge != None:
+            query_search = query_search.filter(Actors.age == searchAge)
+        actors = query_search.all()
+        if len(actors) == 0:
+            return {'actors':[]} 
+        return {'actors': [actor.format() for actor in actors]}
+    @staticmethod
+    def read(id):
+        """Get the actor for an id
+        Keyword arguments:
+            id -- the integer id to search
+        """
+        item = Actors.query.get(id)
+        if item is None:
+            return None
+        return item.response()
+    @staticmethod
+    def read_all(page=0):
+        """Get all actors per page
+        Keyword arguments:
+            page -- the integer page number
+        """
+        items = Actors.query.all()
+        total = len(items)
 
-
+        if page == -1:
+            return { 'actors': [item.format() for item in items], 'total': total, 'count': total}
+        offset = (page-1)*MAX_ACTORS_PER_PAGE
+        if offset > total:
+            return None
+        limit = min(offset + MAX_ACTORS_PER_PAGE,total-1)
+        return { 'actors': [item.format() for item in items][offset:limit], 'total': total, 'count':limit-offset}
 
 class Roles(db.Model):
     __tablename__ = 'roles'
@@ -219,26 +309,5 @@ class Roles(db.Model):
     def __repr__(self):
         return f'{self.format()}'
 
-'''
-Wrappers
 
-'''
-class Search():
-    def search_movies(searchTerm):
-        """Get the entire movies list for a certain search word
-        Keyword arguments:
-            searchTerm -- the String term to search in the name of the movies
-        """
-        search = f'%{searchTerm}%'
-        #query the movie with WHERE LIKE case insentive
-        movies = Movies.query.filter(Movies.name.ilike(search)).all()
-        if len(movies) == 0:
-            return {'movies':[]}
-        return { 'movies': [movie.format() for movie in movies] }
-    def search_actors(searchName,searchGender,searchAge):
-        """Get the entire actor list for a given combination of search terms
-        Keyword arguments:
-            searchName -- the String term to search in the name of the actors
-            searchGender -- the String term to search in the gender of the actors
-            searchAge -- the Integer value to search in the age of the actors
-        """
+
